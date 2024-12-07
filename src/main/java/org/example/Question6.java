0f2fc44ff7b3c53ea9bb9e8c2800c22953cfb1ae
sahilsam05 <153838061+sahilsam05@users.noreply.github.com>
2024-12-07 16:08:26 +0000

    Q6

--- src/main/java/org/example/Question6.java
+++ src/main/java/org/example/Question6.java
@@ -1,15 +1,37 @@
 package org.example;
 
+import java.util.PriorityQueue;
+
 /**
- *  Name:
- *  Class Group:
+ *  Name: Sahil Samuel
+ *  Class Group: SD2A
  */
 
 public class Question6      // Flight take-off (Queue)
 {
-
     public static void main(String[] args)
     {
 
     }
+
+    public void takeoff(String flightCode) {
+        takeoffQueue.add(new Plane(flightCode));
+        System.out.println(flightCode + " is queued for takeoff.");
+    }
+
+    public void land(String flightCode) {
+        landingQueue.add(new Plane(flightCode));
+        System.out.println(flightCode + " is queued for landing.");
+    }
+
+    public void next() {
+        if (!landingQueue.isEmpty()) {
+            Plane flight = landingQueue.poll();
+            System.out.println("Landing " + flight.getFlightCode());
+        } else if (!takeoffQueue.isEmpty()) {
+            Plane flight = takeoffQueue.poll();
+            System.out.println("Taking off " + flight.getFlightCode());
+        } else {
+            System.out.println("No planes waiting.");
+        }
 }
