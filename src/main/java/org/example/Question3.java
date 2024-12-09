c95d8588eb1b8b8957956b7ab8e3822145b4af58
sahilsam05 <153838061+sahilsam05@users.noreply.github.com>
2024-12-09 19:31:17 +0000

    working on  q 3

--- src/main/java/org/example/Question3.java
+++ src/main/java/org/example/Question3.java
@@ -1,11 +1,13 @@
 package org.example;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.Stack;
 
 /**
- *  Name:
- *  Class Group:
+ *  Name: Sahil Samuel
+ *  Class Group: SD2A
  */
 public class Question3  {   //Nested HTML (Stack)
 
@@ -14,6 +16,19 @@ filename: name of the file to test.
 */
     public static boolean validate(String filename) throws FileNotFoundException
     {
+        Stack<String> stack = new Stack<>();
+        Scanner keyboard = new Scanner(new File(filename));
+
+
+        while (keyboard.hasNext())
+        {
+            String tag = keyboard.next();
+
+            if (isOpeningTag(tag))
+            {
+
+            }
+        }
         return false;
     }
 
