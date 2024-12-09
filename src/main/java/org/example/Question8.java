a9454372478509b63fa3a78bbef7ffd4de212b16
sahilsam05 <153838061+sahilsam05@users.noreply.github.com>
2024-12-09 13:53:08 +0000

    Q8 finished

--- src/main/java/org/example/Question8.java
+++ src/main/java/org/example/Question8.java
@@ -1,5 +1,9 @@
 package org.example;
 
+// Reference
+// https://github.com/logued/oop-maps-demo-2022/blob/master/src/main/java/AppMainMapsDemo.java
+// https://www.geeksforgeeks.org/java-util-hashmap-in-java-with-examples/
+
 import java.util.*;
 /**
  *  Name: Sahil Samuel
@@ -18,7 +22,7 @@ public class Question8  // Multi-company (Queue)
     public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
-        Map<String, Queue<Block>> stockMap = new HashMap<String, Queue<Block>>();
+        Map<String, Queue<Block>> sharesMap = new HashMap<String, Queue<Block>>();
         String command="";
         do {
             System.out.print(">");
@@ -29,13 +33,48 @@ public class Question8  // Multi-company (Queue)
                 int qty = in.nextInt();
                 double price = in.nextDouble();
                 // Code to buy shares here
+                sharesMap.putIfAbsent(company, new LinkedList<>());
+                sharesMap.get(company).add(new Block(qty, price));
+                System.out.println(company + " bought " + qty + " " + price);
             }
             else if(command.equals("sell"))
             {
                 String company = in.next();
                 int qty = in.nextInt();
                 double price = in.nextDouble();
+                double totalProfit = 0;
                 // Code to sell shares and calculate profit here
+                Queue<Block> companyShares = sharesMap.get(company); // Get the queue for the company
+                if (companyShares == null || companyShares.isEmpty())
+                {
+                    System.out.println("No shares available");
+                    return;
+                }
+
+                while(qty > 0 && !companyShares.isEmpty())
+                {
+                    Block b = companyShares.remove();
+                    if (b != null) {
+                        if (b.getQuantity() <= qty) {
+                            double profit = (price * b.getPrice()) / b.getQuantity();
+                            totalProfit += profit;
+                            System.out.println(company + " bought " + qty + " " + profit);
+
+                            b.setQuantity(b.getQuantity() - qty);
+                            companyShares.add(b);
+                            qty = 0;
+
+                        }
+                    }
+                }
+                if (qty == 0)
+                {
+                    System.out.println("No shares available");
+                }
+                else
+                {
+                    System.out.println("Total profit: " + String.format("%.2f", totalProfit));
+                }
             }
         }while(!command.equalsIgnoreCase("quit"));
     }
