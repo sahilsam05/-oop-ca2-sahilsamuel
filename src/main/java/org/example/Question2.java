9c59fd7ec339da49f5298c8d55290937359449c1
sahilsam05 <153838061+sahilsam05@users.noreply.github.com>
2024-12-09 18:58:01 +0000

    Completed q2

--- src/main/java/org/example/Question2.java
+++ src/main/java/org/example/Question2.java
@@ -3,6 +3,9 @@ package org.example;
 import java.util.Scanner;
 import java.util.Stack;
 
+
+// Reference
+// Last In First Out Video https://www.youtube.com/watch?v=ulKem8Bpw9M
 /**
  *  Name: Sahil Samuel
  *  Class Group: SD2A
@@ -15,6 +18,27 @@ public class Question2  // Car Parking - Stack
         Stack<Integer> street = new Stack<Integer>();
 
         Scanner keyboard = new Scanner(System.in);
+
+        while (true)
+        {
+            System.out.print("Enter a number  (positive to park, negative to retrieve, 0 to exit) : ");
+            int number = keyboard.nextInt();
+
+            if  (number == 0)
+            {
+                System.out.println("Simulation ended! ");
+            }
+            else if(number > 0)
+            {
+                driveway.push(number);
+                System.out.println("Car " + number + " parked.");
+            }
+            else
+            {
+                int targetCar = driveway.pop();
+            }
+        }
+
     }
 
     public static void main(String[] args) {
